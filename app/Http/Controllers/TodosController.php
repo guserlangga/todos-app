17aa5558f42ea7d0eb90ffee53f5cb4fcde67555
Gusega <baguserlangga96@gmail.com>
2020-12-19 23:55:01 +0800

    init

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    public function index() {
+        return view('todos.index')->with('todos', Todo::all());
+    }
+
+    public function show($todoId) {
+        return view('todos.show')->with('todo', Todo::find($todoId));
+    }
+
+    public function create() {
+        return view('todos.create');
+    }
+
+    public function store() {
+        
+        $this->validate(request(), [
+            'name' => 'required|min:4|max:24',
+            'description' => 'required'
+        ]);
+        $data = request()->all();
+
+        $todo = new Todo();
+        $todo->name = $data['name'];
+        $todo->description = $data['description'];
+        $todo->completed = false;
+
+        $todo->save();
+
+        return redirect('/todos');
+    }
+
+    public function edit($todoId) {
+        $todo = Todo::find($todoId);
+
+        return  view('todos.edit')->with('todo', $todo);
+    }
+    
+    public function update($todoId) {
+        $this->validate(request(), [
+            'name' => 'required|min:4|max:24',
+            'description' => 'required'
+        ]);
+
+        $data = request()->all();
+
+        $todo = Todo::find($todoId);
+
+        $todo->name  = $data['name'];
+        $todo->description = $data['description'];
+
+            $todo->save();
+
+        return redirect('/todos');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Todos</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/todos">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/new-todos">Create Todos</a>
+                </li>
+            </ul>            
+            </div>
+        </div>
+        </nav>
+
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
